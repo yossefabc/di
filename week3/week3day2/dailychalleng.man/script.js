da3153c31f9d657ad5be21c926738cbab5c16913
yossefabc <yossefbekele30@gmail.com>
2023-12-01 10:46:20 +0200

    main

--- week3/week3day2/dailychalleng.man/script.js
+++ week3/week3day2/dailychalleng.man/script.js
@@ -2,17 +2,19 @@
 
 // 1.Create a variable called sentence. The value of the variable should be a string that contains the words “not” and “bad”.
 // For example, “The movie is not that bad, I like it”.
- let sentence='This movie is not so bad !'
+ let sentance= "This movie is not that  bad, i like it";
 // 2.Create a variable called wordNot where it’s value is the first appearance (ie. the position) of the substring “not” (from the sentence variable).
-let wordNot=3;
+let wordnot=sentance.search("not");
+console.log(wordnot);
 // 3.Create a variable called wordBad where it’s value is the first appearance (ie. the position) of the substring “bad” (from the sentence variable).
-let wordBad=5;
+let wordbad=sentance.search("bad");
+console.log(wordbad);
 // 4.If the word “bad” comes after the word “not”, you should replace the whole “not…bad” substring with “good”, then console.log the result.
 // For example, the result here should be : “The movie is good, I like it”
-if(wordBad>3){
-    console.log("this movie is good")
+if(wordbad > wordnot){
+    let result= sentance.replace("not that good","good");
+    console.log(result);
+} else if (wordbad < wordnot) {
+    console.log(sentance);
 }
 // 5.If the word “bad” does not come after “not” or the words are not in the sentence, console.log the original sentence.
-else if(wordBad<3){
-    console.log(sentence)
-}
---
+++ week3/week3day3/exercise.xp.man/script.js
@@ -0,0 +1,44 @@
+// Exercise 1 : List Of People
+
+// const people = ["Greg", "Mary", "Devon", "James"];
+
+// Part I - Review About Arrays
+
+// 1.Write code to remove “Greg” from the people array.
+
+const people=["Greg", "Mary", "Devon", "James"]
+console.log(people);
+console.log(people.shift());
+console.log(people)
+
+
+
+
+
+//2.Write code to replace “James” to “Jason”.
+   people.splice(2,1,"Jason");
+console.log(people);
+
+
+// 3.Write code to add your name to the end of the people array.
+
+people.push("yossef");
+console.log(people);
+
+
+
+
+// 4.Write code that console.logs Mary’s index
+ 
+console.log(people.indexOf("Mary"));
+
+//5.Write code to make a copy of the people array using the slice method
+
+
+console.log(people.slice(1,3));
+
+//6.Write code that gives the index of “Foo”. Why does it return -1 ?
+
+let fo="foo"
+console.log(fo.indexOf("foo"));
+
