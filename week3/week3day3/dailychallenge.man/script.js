56106351968094f89858712735b52675fe2181f2
yossefabc <yossefbekele30@gmail.com>
2023-12-03 17:41:28 +0200

    main

--- week3/week3day3/dailychallenge.man/script.js
+++ week3/week3day3/dailychallenge.man/script.js
@@ -1,2 +1,15 @@
-console.log(hiiii)
+
+// Daily Challenge: Stars
+
+//1.
+let marks = {
+    a : "*",ab:"**",abc:"***",abcd:"****",
+    abcde:"*****",abcdef:"******",
+};
+for(let x in marks) {
+    console.log(marks[x]);
+}
+//2.
+
+
 
