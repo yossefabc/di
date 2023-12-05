670c5ec832e8735b2a936bf1542cfc8ab36a370a
yossefabc <yossefbekele30@gmail.com>
2023-12-05 09:33:18 +0200

    main

--- week4/day1/exercise.xp.man/script.js
+++ week4/day1/exercise.xp.man/script.js
@@ -27,11 +27,22 @@ function calculateTip(){
     prompt("john what the amount of the bill")
 }
 //3.
-// function calculateTip(){
-//     prompt("john what the amount of the bill")
+function calculateTip(){
+    prompt("john what the amount of the bill");
+    for(let i=0; i>0; i++){
+        if(i>$50){
+            prompt("tip 20%");
+        }else if($50<i<$200){
+            prompt("tip 15%");
+        }else if(i>$200){
+            prompt("tip 10%");
+        }
+    console.log(i);
+    }
+
     
     
-// }  
+}  
 // calculateTip(); 
 
 
@@ -40,11 +51,55 @@ function calculateTip(){
 // Exercise 3 : Find The Numbers Divisible By 23
 //1.
 function  isDivisible(){}
-//2.
+//2.3.4
 function  isDivisible(){
- for(   i=0;i<500;i++){
-
- }
-
+    
+    for(let i=0;i <= 500  ;i++){
+     if(i % 23 ===0){
+        console.log(i);
+      
+        
+      
+     } 
+        
+     
+     
+      
 }
+}
+  isDivisible()
 
+ 
+
+
+//   Exercise 4 : Shopping List
+//1.
+const stock = { 
+    "banana": 6, 
+    "apple": 0,
+    "pear": 12,
+    "orange": 32,
+    "blueberry":1
+}  
+
+const prices = {    
+    "banana": 4, 
+    "apple": 2, 
+    "pear": 1,
+    "orange": 1.5,
+    "blueberry":10
+} 
+
+//2.
+const shoppingList=["banana","orange","apple"];
+
+//3.
+function myBill(){} 
+
+//4.
+function myBill(){
+   
+       console.log(prices.banana+prices.apple+prices.orange);
+   
+}
+myBill()
