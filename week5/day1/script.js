4111f3bda928a2b8c46c438009ec9b06a11619a9
yossefabc <yossefbekele30@gmail.com>
2023-12-11 21:58:57 +0200

    main

---
+++ week5/day1/script.js
@@ -0,0 +1,72 @@
+// // Exercise 1 : Users
+
+// //1.
+// //2.
+//    //1. 
+//    const containerid=document.getElementById("container");
+//    console.log(containerid);
+//    //.2
+//    document.getElementsByClassName("list").textContent = "users";
+// //    document.getElementsByTagName("li")[1].innerHTML = "Richard"
+
+
+// //3.
+    
+// const x = document.getElementById("sarah");
+// x.remove();
+// ///4.
+// const y = document.querySelectorAll("ul>li");
+// for(let x of y){
+//     document.getElementsByTagName("li")[0].innerHTML = "yossef";
+//     document.getElementsByTagName("li")[2].innerHTML = "yossef";
+// }
+
+
+//Exercise 2 : Users And Style
+  //2.
+     //1.
+// const div = document.getElementById("user");
+// div.style.backgroundColor= "lightblue";
+// div.style.padding= "10px";
+//      //2.
+//      const li = document.querySelector("li");
+//      li.remove()
+//      //3.
+//      const red = document.querySelector("li");
+//      red.style.border = "1px solid red";
+//      //4.
+//      const body = document.querySelector("body");
+//      body.style.fontSize = "50px";
+
+
+    //  Exercise 3 : Change The Navbar
+
+    // //2.
+    // const div = document.querySelector("div");
+    // div.setAttribute("navbar","socialNetworkNavigation");
+    // //.3
+    //    //1.2.3.4
+    //    const list = document.createElement("li");
+    //    list.innerText= "logout";
+    //    document.body.appendChild(list);
+    // //4.
+
+    //     let text = document.getElementsByName("profile").textContent;
+    //     document.getElementById("navbar").innerHTML = text;
+
+
+        // Exercise 4 : My Book List
+
+
+        //2.1.3
+      const  allBooks= {Harrypotter:"JKRolling",
+      Moneytalks:"Yossefbk"
+       
+};
+
+         //4.
+            
+          document.getElementById("listbooks").innerHTML = "allbooks";
+         
+
+           
