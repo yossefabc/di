cd47747b8ab870b1179adc3c68d7baea7319df5c
yossefabc <yossefbekele30@gmail.com>
2023-12-28 19:17:37 +0200

    main

---
+++ week7/week7day3-dailly.challenge.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE actors(
+--  actor_id SERIAL PRIMARY KEY,
+--  first_name VARCHAR (50) NOT NULL,
+--  last_name VARCHAR (100) NOT NULL,
+--  age DATE NOT NULL,
+--  number_oscars SMALLINT NOT NULL
+-- )
+
+-- SELECT * FROM actors;
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('Matt','Damon','08/10/1970', 5),('George','Clooney','06/05/1961', 2);
+--1.
+-- SELECT count(actors) AS name_actor FROM actors;
+--2.
+-- INSERT INTO actors (first_name,last_name,age,number_oscars)
+--             VALUES('yossef','bekele','1/1//2000' ,'20');
