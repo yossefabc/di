54754a3e39e6cb3232afa69b4e9862135f554efb
yossefabc <yossefbekele30@gmail.com>
2024-01-31 16:03:38 +0200

    main

--- week12/day1/daily.challenge/script.js
+++ week12/day1/daily.challenge/script.js
@@ -2,6 +2,7 @@
 
 
 const from = document.getElementById("from")
+console.log(from)
 const to = document.getElementById("to")
 const amount = document.getElementById("amount")
 const sum = document.getElementById("sum")
@@ -13,3 +14,20 @@ function handleclick(){
 }
 
 handleclick()
+
+
+const changecurrency= async()=>{
+    try{
+        const res= await fetch("  https://v6.exchangerate-api.com/v6/bbeece1c18ec160923458499/latest/USD")
+        if(res.ok){
+            const data= await res.json()
+            console.log(data.conversion_rates
+                );
+            
+        }
+    }catch(e){
+        console.log(e)
+    }
+}
+
+changecurrency()
