931ac43920f1850a3bfcf4353d5dc8b1747b8c6c
yossefabc <yossefbekele30@gmail.com>
2024-03-12 16:21:11 +0200

    main

---
+++ week18/day1/mini-project/vite-project/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import TodoForm  from './components/TodoForm';
+import TodoItem from './components/TodoItem';
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <TodoItem
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ week18/day1/mini-project/vite-project/src/components/TodoItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TodoItem({ todo, index, completeTodo, removeTodo }) {
+    return (
+      <div
+        className="todo"
+        style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
+      >
+        {todo.text}
+        <div>
+          <button onClick={() => completeTodo(index)}>Complete</button>
+          <button onClick={() => removeTodo(index)}>Delete</button>
+        </div>
+      </div>
+    );
+  }
+
+  export default TodoItem;
